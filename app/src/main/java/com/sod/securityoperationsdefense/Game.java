f3f46e41531d17afc4eb97bb6b2b1b4fd2d7c05e
Dylan Pryor <pryor.169@osu.edu>
2021-03-14 17:35:09 -0400

    actually made the game class this time

--- app/src/main/java/com/sod/securityoperationsdefense/Game.java
+++ app/src/main/java/com/sod/securityoperationsdefense/Game.java
@@ -1,4 +1,13 @@
 package com.sod.securityoperationsdefense;
 
 public class Game {
+    public double currentFunds;
+    public double payRate;
+    public int payDelay;
+
+    public Game() {
+        this.currentFunds = 1000.0;
+        this.payRate = 25.0;
+        this.payDelay = 1; // one second
+    }
 }
